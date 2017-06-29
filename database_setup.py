dcd10b661a0f29d07270cf25c5eef6f15c7bf9df
Zachary Caldwell <zachary.austin.caldwell@gmail.com>
2017-06-28 22:58:33 -0400

    Move statement to one line

--- database_setup.py
+++ database_setup.py
@@ -47,8 +47,7 @@ class MenuItem(Base):
 	course = Column(String(250))
 	description = Column(String(250))
 	price = Column(String(8))
-	restaurant_id = Column(
-	Integer, ForeignKey('restaurant.id'))
+	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
 	restaurant = relationship(Restaurant)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
