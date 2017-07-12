b412d3af2a8aa3c2f889e7d580c83d62401a6dd7
Zachary Caldwell <zachary.austin.caldwell@gmail.com>
2017-07-12 15:38:50 -0400

    Complete code to return all puppies in alphabetical order

--- puppy_query.py
+++ puppy_query.py
@@ -10,11 +10,14 @@ engine = create_engine('sqlite:///puppyshelter.db', echo=False)
 # Query all of the puppies and return the results
 # in ascending alphabetical order
 conn = engine.connect()
-all_puppies = select([Puppy.label('pup')]).order_by("pup")
+all_puppies = select([Puppy.name]).order_by("name")
 result = conn.execute(all_puppies)
 row = result.fetchone()
 
 for row in result:
-	print row
+	print row[0]
 
 conn.close()
+
+# Query all of the puppies that are less than 6 months old organized by the youngest first
+
